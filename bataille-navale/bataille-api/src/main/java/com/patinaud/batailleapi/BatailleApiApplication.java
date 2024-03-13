4e9287eef7f8602278688a432c2f751821e241ae
Kévin Patinaud <kevin@patinaud.org>
2024-03-13 17:15:57 +0100

    add websocket

---
+++ bataille-navale/bataille-api/src/main/java/com/patinaud/batailleapi/BatailleApiApplication.java
@@ -0,0 +1,13 @@
+package com.patinaud.batailleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatailleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatailleApiApplication.class, args);
+    }
+
+}
---
+++ bataille-navale/bataille-api/src/test/java/com/patinaud/batailleapi/BatailleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patinaud.batailleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatailleApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
